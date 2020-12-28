0d02b6700182f29d4f2180c1135a18bd2a934a3c
Adel Makhloufi <adel.makhloufi@icloud.com>
2020-12-27 19:59:59 -0500

    Creating project files

---
+++ wrapper/include/io.h
@@ -0,0 +1,4 @@
+#ifndef _NL_ARDUINO_IO_H
+#define _NL_ARDUINO_IO_H
+
+#endif
---
+++ wrapper/samples/led-blink/include/led-blink.h
@@ -0,0 +1,6 @@
+#ifndef _NL_ARDUINO_SAMPLES_LED_BLINK
+#define _NL_ARDUINO_SAMPLES_LED_BLINK
+
+#include "io.h"
+
+#endif
